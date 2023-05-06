e0960f235eb3445ddf958db4636ca9ba65bbc367
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-05-06 10:53:56 -0400

    Updated and Complete LAB

--- index.js
+++ index.js
@@ -6,38 +6,42 @@ class SortedList {
 
   add(item) {
     this.items.push(item);
-    this.items.sort((a, b) => a - b);
+    this.items.sort((a, b) => {
+      if (a > b) {
+        return 1
+      } else if (a < b) {
+        return -1
+      } else return 0
+    });
     this.length = this.items.length;
     
   }
 
   get(pos) {
-    if (pos > this.items.length) {
+    if (!this.items[pos]) {
       throw new Error('OutOfBounds');
     }
     return this.items[pos];
   }
 
   max() {
-    if (this.length === 0) {
+    if (!this.length) {
       throw new Error('EmptySortedList');
     } 
-      return Math.max(...this.items);
-    
-
-
-    
+      return Math.max(...this.items);       
+      // if our values are not numbers:
+      // return this.items[this.items.length - 1];
   }
 
   min() {
-    if (this.length === 0) {
+    if (!this.length) {
       throw new Error('EmptySortedList');
     } 
       return Math.min(...this.items);
   }
 
   sum() {
-    if (this.length === 0) {
+    if (!this.length) {
       return 0
     } else {
       return this.items.reduce((a, b) => a +  b)
@@ -46,7 +50,7 @@ class SortedList {
   }
 
   avg() {
-    if (this.length === 0) {
+    if (!this.length) {
       throw new Error('EmptySortedList');
     } else {
     return this.sum() / this.items.length;
